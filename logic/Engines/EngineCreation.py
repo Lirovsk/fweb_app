e482037ea624f8bf8f98541f5067da253dbaa1e8
João Vítor Lira <16088480602@estudante.ufjf.br>
2025-07-24 11:20:11 -0300

    Creating the .gitignore

--- logic/Engines/EngineCreation.py
+++ logic/Engines/EngineCreation.py
@@ -4,8 +4,35 @@ from Models import EngineStorage
 from Models import GameRoom
 from Models import Base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from datetime import datetime
 
 
-engine = create_engine('sqlite:///engine_storage.db')
-Base.metadata.create_all(engine)
+engine_for_storage = create_engine('sqlite:///engine_storage.db')
+EngineStorage.__table__.create(engine_for_storage)
 
+def create_engine(room_name: str, pin: str, engine_type = "sqlite") -> EngineStorage:
+    """Create a new engine storage entry."""
+    with Session(engine_for_storage) as session:
+        new_engine = EngineStorage(room_name=room_name, pin=pin, engine=engine_type)
+        session.add(new_engine)
+        session.commit()
+        return new_engine
+    
+def delete_engine() -> None:
+    """Delete an room if its not in use."""
+    with Session(engine_for_storage) as session:
+        results =session.query(EngineStorage).all()
+        for engine in results:
+            if (engine.last_update - datetime.now()) < datetime.timedelta(minutes=40):
+                session.delete(engine) 
+        session.commit()
+        
+def retrieve_engine(room_name: str) -> EngineStorage:
+    """Retrieve an engine storage entry by room name."""
+    try:
+        with Session(engine_for_storage) as session:
+            return session.query(EngineStorage).filter_by(room_name=room_name).first()
+    except Exception as e:
+        msg = (f"Error retrieving engine: {e}")
+        return msg
