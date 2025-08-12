ac26b848fe0ee1cb21869646cd174f8aed66384b
lirovsk <joao.lira@engenharia.ufjf.br>
2025-08-12 12:03:23 -0300

    fix(__ini__) now, all the paths and files (as databases) are checked during the initialization process

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import (Flask,
     render_template,
     request)
 from .Engines.GeneralServices import insp_default_storage, create_default_storage
+from pathlib import Path
+from .paths import ROOM_PATH   
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -25,6 +27,13 @@ def create_app(test_config=None):
             except Exception as e:
                 print(f"Error creating default storage: {e}")
     
+    #this ensures the path to the directory exists
+    if ROOM_PATH.exists(): #check if the rooms directory exists
+        pass    
+    else:
+        ROOM_PATH.mkdir(parents=True, exist_ok=True) #create the directory in case it doesn't exist
+
+
     if test_config is None:
         # Load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
